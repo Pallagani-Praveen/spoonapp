e8bb31512486f407e881525d1c154f6ed7b1be2b
Pallagani-Praveen <praveen.p18@iiits.in>
2020-06-28 19:59:06 +0530

    delete option and its functionality added

--- index.js
+++ index.js
@@ -14,18 +14,44 @@
   const db = firebase.firestore();
   const spnRef = db.collection('spoons');
 
+  // ultility function
+  // const removeDoc = id =>{
+  //   const li = document.querySelector('[data-id="'+id+'"]');
+  //   const ul = document.querySelector('.list-spoons');
+  //   ul.removeChild(li);
+  //
+  // }
+
 function renderSpoon(doc){
     const ul = document.querySelector('.list-spoons');
     const li = document.createElement('li');
     const name = document.createElement('span');
     const city = document.createElement('span');
     const br = document.createElement('br');
+    const cross = document.createElement('div');
+    cross.textContent = 'X';
+    cross.setAttribute('class','cross');
+    li.setAttribute('data-id',doc.id);
     name.textContent = 'Name : '+doc.data().name;
     city.textContent = 'Body : '+doc.data().body;
     li.appendChild(name);
+    li.appendChild(cross);
     li.appendChild(br);
     li.appendChild(city);
     ul.appendChild(li);
+
+    cross.addEventListener('click',(e)=>{
+      let id = e.target.parentElement.getAttribute('data-id');
+      spnRef.doc(id).delete().then((err)=>{
+        if(err){
+          console.log(`error : ${err}`);
+        }
+        else{
+          // removeDoc(id); // this function will update the page only after the recored deleted in db
+          console.log('record deleted successfully');
+        }
+      });
+    });
 }
 
   // not a real time data getting function
@@ -39,10 +65,21 @@ function renderSpoon(doc){
   spnRef.onSnapshot(snapShot=>{
     const changes = snapShot.docChanges();
     changes.forEach((change) => {
+      if(change.type==='added'){
       renderSpoon(change.doc);
+      }
+      else if(change.type==='removed'){
+        const li = document.querySelector('[data-id="'+change.doc.id+'"]');
+        const ul = document.querySelector('.list-spoons');
+        ul.removeChild(li);
+       // this function gives immidiate update on the page
+      }
+
     });
 
   });
+
+
 
 
   const saveButton = document.querySelector('.form button');
